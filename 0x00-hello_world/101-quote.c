44d5f2b25f9de156ed2c6facf9b78610679ef681
AhmedOT22 <ahmedouazzani225@gmail.com>
2023-03-16 16:11:34 -0700

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	while (*str)
+	{
+		write(STDERR_FILENO, str++, 1);
+	}
+	return (1);
+}
