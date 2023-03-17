4c0122d4bb6188f44da2d461271bfc4ddcf7ee56
AhmedOT22 <ahmedouazzani225@gmail.com>
2023-03-16 17:40:40 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+
 	while (*str)
 	{
 		write(STDERR_FILENO, str++, 1);
 	}
+
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 /**
- * This is a program that prints the size of various types on the computer.
+ * main - A program that prints the size of various types on the computer it is
+ * compiled and run on.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("The size of char is %ld bytes\n", sizeof(char));
-	printf("The size of short is %ld bytes\n", sizeof(short));
-	printf("The size of int is %ld bytes\n", sizeof(int));
-	printf("The size of long is %ld bytes\n", sizeof(long));
-	printf("The size of long long is %ld bytes\n", sizeof(long long));
-	printf("The size of float is %ld bytes\n", sizeof(float));
-	printf("The size of double is %ld bytes\n", sizeof(double));
-	printf("The size of long double is %ld bytes\n", sizeof(long double));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
