e495f0975079d85770d2bc9fb0334f35273ce3b1
AhmedOT22 <ahmedouazzani225@gmail.com>
2023-03-16 17:29:31 -0700

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-	printf("The size of char is %d bytes\n", sizeof(char));
-	printf("The size of short is %d bytes\n", sizeof(short));
-        printf("The size of int is %d bytes\n", sizeof(int));
-        printf("The size of long is %d bytes\n", sizeof(long));
-        printf("The size of long long is %d bytes\n", sizeof(long long));
-        printf("The size of float is %d bytes\n", sizeof(float));
-        printf("The size of double is %d bytes\n", sizeof(double));
-        printf("The size of long double is %d bytes\n", sizeof(long double));
-        return (0);
+	printf("The size of char is %ld bytes\n", sizeof(char));
+	printf("The size of short is %ld bytes\n", sizeof(short));
+	printf("The size of int is %ld bytes\n", sizeof(int));
+	printf("The size of long is %ld bytes\n", sizeof(long));
+	printf("The size of long long is %ld bytes\n", sizeof(long long));
+	printf("The size of float is %ld bytes\n", sizeof(float));
+	printf("The size of double is %ld bytes\n", sizeof(double));
+	printf("The size of long double is %ld bytes\n", sizeof(long double));
+
+	return (0);
 }
