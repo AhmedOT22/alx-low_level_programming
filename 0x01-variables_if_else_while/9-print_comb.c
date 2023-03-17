86e6a4c8ffcfaae21d9fce37f5ad91210b4eaf1b
AhmedOT22 <ahmedouazzani225@gmail.com>
2023-03-17 14:55:21 -0700

    m

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar(i + '0');
+	return (0);
+}
