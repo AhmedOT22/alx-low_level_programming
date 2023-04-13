1f4ed11b3e27da408da1de7cff9d687bfca370f6
AhmedOT22 <ahmedouazzani225@gmail.com>
2023-04-12 19:55:32 -0700

    Done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,8 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	p = (char *) malloc(size * sizeof(char));
-
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = c;
 
