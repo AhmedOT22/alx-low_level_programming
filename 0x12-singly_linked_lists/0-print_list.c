3b48fbafc26c06c0e3e20e91ab0c66e569a0f07d
AhmedOT22 <ahmedouazzani225@gmail.com>
2023-04-30 06:53:27 -0700

    Done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,8 +19,8 @@ size_t print_list(const list_t *h)
 			printf("[%u], %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
-		nodes_nb++;
 		h = h->next;
+		nodes_nb++;
 	}
 
 	return (nodes_nb);
