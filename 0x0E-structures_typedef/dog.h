e3824d0b13865a4a69626c9a65dbaf144cd30af7
AhmedOT22 <ahmedouazzani225@gmail.com>
2023-04-30 13:21:27 -0700

    Done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,18 +11,12 @@
  * Description: structure representing basic info of dogs
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - typedef for the dog structure
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
